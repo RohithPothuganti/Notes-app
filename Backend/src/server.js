1f94b5606a191eb62bdbf8ea13832f1a765c9221
Pothuganti Rohith <149678592+RohithPothuganti@users.noreply.github.com>
2025-08-17 10:51:44 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -14,17 +14,17 @@ const __dirname = path.resolve(path.dirname(''));
 
 app.use(express.json());
 
-const allowedOrigins = [
-  'http://localhost:5173'
-];
+const allowedOrigins = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : [];
 
 app.use(cors({
   origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
     }
+    return callback(null, true);
   }
 }));
 
